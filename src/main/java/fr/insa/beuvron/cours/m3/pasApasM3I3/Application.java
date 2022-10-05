415e83f2065b162df14f1060b7728eedae10d47c
francois <francois@PortableFdB>
2022-10-05 12:54:51 +0200

    passage a vaadin 23.2.3

---
+++ src/main/java/fr/insa/beuvron/cours/m3/pasApasM3I3/Application.java
@@ -0,0 +1,44 @@
+/*
+    Copyright 2000- Francois de Bertrand de Beuvron
+
+    This file is part of CoursBeuvron.
+
+    CoursBeuvron is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CoursBeuvron is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CoursBeuvron.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.insa.beuvron.cours.m3.pasApasM3I3;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "mytodo")
+// @PWA(name = "My Todo", shortName = "My Todo", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
